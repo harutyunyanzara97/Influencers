ec463fdb2bbff1745427907a0e3bea48359e535d
codelover2021 <codelover2021@gmail.com>
2021-07-07 14:12:20 +0400

    project

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'instagram' => \App\Http\Middleware\InstagramAPIMiddleware::class,
+        'influencer' => \App\Http\Middleware\IsInfluencer::class,
     ];
 }
